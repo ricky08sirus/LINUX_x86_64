58dbf5550f75e8f9fa6aa791410d3d5d1a4b84b5
Amandeep yadav <aman08jul@gmail.com>
2024-12-24 11:40:03 +0530

    initial configuration

---
+++ LFS/lfs.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+export LFS=/mnt/lfs
+export LFS_TGT=x86_64-lfs-linux-gnu
+export LFS_DISK=/dev/sda1
+#check if the file is munted or not , all mounted files can be checked on /proc/mounts/
+
+if ! grep -q "$LFS" /proc/mounts; then
+  source setupdisk.sh "$LFS_DISK"
+  #as the patition has been created mount it to use it b a system
+  sudo mount "$(LFS_DISK)2" "LFS"
+  #change the owner to myself
+  sudo chown -v $USER "$LFS"
+fi
+#this is where intermidiate cross compiler compiling from A to B will sit
+mkdir -pv $LFS/sources
+mkdir -pv $LFS/tools
+
+mkdir -pv $LFS/boot
+mkdir -pv $LFS/etc
+mkdir -pv $LFS/bin
+mkdir -pv $LFS/lib
+mkdir -pv $LFS/sbin
+mkdir -pv $LFS/usr
+mkdir -pv $LFS/var
+
+case $(uname -m) in
+x86_64) mkdir -pv $LFS/lib64 ;;
+esac
---
+++ LFS/setupdisk.sh
@@ -0,0 +1,22 @@
+#fdisk is not user friendly but its a command line so it's a good idea to use it here
+LFS_DISK="$1"
+sudo fdisk "$LFS_DISK" <<EOF
+o
+n 
+p 
+1
+
++100M
+n 
+p 
+2 
+
+
+p 
+w 
+q 
+EOF
+
+#after the partitions we will make fle systems
+sudo mkfs -t ext2 -F "${LFS_DISK}1"
+sudo mkfs -t ext2 -F "${LFS_DISK}2"
